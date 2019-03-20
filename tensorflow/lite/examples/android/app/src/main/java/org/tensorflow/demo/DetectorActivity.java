0b1139ac4e879bf9d619120cab942b3dc98a9dbc
MyungSung Kwak <yesmung@gmail.com>
2019-03-20 18:19:56 +0900

    Modify add
    Signed-off-by: MyungSung Kwak <yesmung@gmail.com>

--- tensorflow/lite/examples/android/app/src/main/java/org/tensorflow/demo/DetectorActivity.java
+++ tensorflow/lite/examples/android/app/src/main/java/org/tensorflow/demo/DetectorActivity.java
@@ -49,10 +49,10 @@ public class DetectorActivity extends CameraActivity implements OnImageAvailable
   private static final Logger LOGGER = new Logger();
 
   // Configuration values for the prepackaged SSD model.
-  private static final int TF_OD_API_INPUT_SIZE = 300;
+  private static final int TF_OD_API_INPUT_SIZE = 480;
   private static final boolean TF_OD_API_IS_QUANTIZED = true;
-  private static final String TF_OD_API_MODEL_FILE = "detect.tflite";
-  private static final String TF_OD_API_LABELS_FILE = "coco_labels_list.txt";
+  private static final String TF_OD_API_MODEL_FILE = "detect_float.tflite";
+  private static final String TF_OD_API_LABELS_FILE = "labels_list.txt";
 
   // Which detection model to use: by default uses Tensorflow Object Detection API frozen
   // checkpoints.
